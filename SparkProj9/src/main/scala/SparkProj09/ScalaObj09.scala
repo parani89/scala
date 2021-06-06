9ae641541085f4100cef9b657f10c44379809a64
Parani <Parani@Parani>
2021-06-06 23:58:33 +0530

    first commit

---
+++ SparkProj9/src/main/scala/SparkProj09/ScalaObj09.scala
@@ -0,0 +1,27 @@
+package SparkPack09
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import sys.process._
+
+object SparkObj09 {
+
+	def main(args:Array[String]):Unit={
+
+			val conf = new SparkConf().setAppName("ES").setMaster("local[*]")
+					val sc = new SparkContext(conf)
+					sc.setLogLevel("ERROR")
+
+					val data = sc.textFile("file:///home/cloudera/data/txns")
+
+
+					val basketdata = data.filter(x=>x.contains("Basketball"))
+
+
+					"hadoop fs -rmr /user/cloudera/basketball_dir_dply" !
+
+
+					basketdata.saveAsTextFile("hdfs:/user/cloudera/basketball_dir_dply")
+
+	}
+}
